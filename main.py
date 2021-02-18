0629a9c2254fd76c0a11a6a021cdc5d38bddfc2b
jaredgoodin <813552@mystma.org>
2021-02-18 15:52:53 +0000

    planets try 1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#WRITE YOUR CODE IN THIS FILE
+def planets(p): 
+
+    for i in range(0, len(planet)):
+        if p == planet[i]:
+            return i
+        
+    return p + "is not a planet"
+    
+    
+    
+planet = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+print (planets("earth"))
